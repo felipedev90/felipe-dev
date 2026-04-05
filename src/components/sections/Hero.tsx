4ded853a356f21467a485758a54445a27c08fa4c
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-05 20:47:57 -0300

    feat: add hero component

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section>
+      <div>
+        <span>Disponível para novos projetos</span>
+        <h1>Sites profissionais que elevam sua marca</h1>
+        <p>
+          Desenvolvimento de sites de alta performance para empresas que buscam
+          crescer online.
+        </p>
+      </div>
+    </section>
+  );
+}
